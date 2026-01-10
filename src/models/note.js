59e82474b3a4baf85b86681a8ad9d95fc114d7db
Olha <olya.nota@gmail.com>
2026-01-10 21:54:30 +0100

    work with router and mongoDB

---
+++ src/models/note.js
@@ -0,0 +1,32 @@
+import { Schema } from 'mongoose';
+
+import { model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    content: { type: String, default: '', trim: true, required: false },
+    tag: {
+      type: String,
+      required: false,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Note = model('Note', noteSchema);
