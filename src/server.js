4b3695264077ac94e3bc07485ef89acefbe2ba62
Olha <olya.nota@gmail.com>
2026-01-10 22:16:25 +0100

    work with router and mongoDB4

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
-import notesRoutes from './routes/notesRoutes';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 
