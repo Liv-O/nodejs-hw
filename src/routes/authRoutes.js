98de601827f5d6386d46cd4befee5e3d5bf5304c
Olha <olya.nota@gmail.com>
2026-02-08 17:59:15 +0100

    added reset password, upload images

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,12 +3,16 @@ import { Router } from 'express';
 import {
   loginUserSchema,
   registerUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
 } from '../validations/authValidation.js';
 import {
   loginUser,
   logoutUser,
   refreshUserSession,
   registerUser,
+  requestResetEmail,
+  resetPassword,
 } from '../controllers/authController.js';
 
 const router = new Router();
@@ -16,6 +20,18 @@ const router = new Router();
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
+
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
+
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
+);
 
 router.post('/auth/refresh', refreshUserSession);
 
--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
-
+import userRoutes from './routes/userRoutes.js';
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
@@ -21,6 +21,7 @@ app.use(cors());
 app.use(cookieParser());
 
 app.use(authRoutes);
+app.use(userRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
